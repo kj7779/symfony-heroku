28b84b7cf0f205bf659d80ed6c5a01c311143d4f
kj7779 <kojistill1212@outlook.jp>
2023-04-19 18:10:39 +0900

    add heroku-postbuild

--- src/Controller/DiaryController.php
+++ src/Controller/DiaryController.php
@@ -9,15 +9,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Knp\Component\Pager\PaginatorInterface;
 
 #[Route('/diary')]
 class DiaryController extends AbstractController
 {
     #[Route('/', name: 'app_diary_index', methods: ['GET'])]
-    public function index(DiaryRepository $diaryRepository): Response
+    public function index(DiaryRepository $diaryRepository,Request $request, PaginatorInterface $paginator): Response
     {
+        $queryBuilder = $diaryRepository->findAll();
+        $pagination = $paginator->paginate(
+            $queryBuilder, /* query NOT result */
+            $request->query->getInt('page', 1)/*page number*/,
+            3/*limit per page*/
+        );
+
         return $this->render('diary/index.html.twig', [
-            'diaries' => $diaryRepository->findAll(),
+            'pagination' => $pagination,
         ]);
     }
 
