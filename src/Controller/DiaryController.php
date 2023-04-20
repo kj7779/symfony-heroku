2a171caaa44d2b5904b88f273d0d0302e73a4d91
kj7779 <kojistill1212@outlook.jp>
2023-04-20 14:15:34 +0900

    adjust textarea

--- src/Controller/DiaryController.php
+++ src/Controller/DiaryController.php
@@ -23,9 +23,15 @@ class DiaryController extends AbstractController
             $request->query->getInt('page', 1)/*page number*/,
             3/*limit per page*/
         );
+        $pagination->setCustomParameters([
+            'align' => 'center', # center|right (for template: twitter_bootstrap_v4_pagination and foundation_v6_pagination)
+            'size' => 'large', # small|large (for template: twitter_bootstrap_v4_pagination)
+            'style' => 'bottom',
+            'span_class' => 'whatever',
+        ]);
 
         return $this->render('diary/index.html.twig', [
-            'pagination' => $pagination,
+            'pagination' => $pagination
         ]);
     }
 
--- src/Form/DiaryType.php
+++ src/Form/DiaryType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Diary;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +14,8 @@ class DiaryType extends AbstractType
     {
         $builder
             ->add('date')
-            ->add('content')
+            ->add('content', TextareaType::class, [
+                'attr' => array('style' => 'width: 100%','rows' => '15')])
         ;
     }
 
