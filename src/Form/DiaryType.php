df6cbb80cca860ffb426caa18d75b7660b6c88f0
kj7779 <kojistill1212@outlook.jp>
2023-04-20 14:36:39 +0900

    form get today

--- src/Form/DiaryType.php
+++ src/Form/DiaryType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Diary;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +13,16 @@ class DiaryType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $dt = new \DateTime();
         $builder
-            ->add('date')
+            ->add('date', DateType::class, [
+                'input'  => 'array',
+                'widget' => 'choice',
+                'format' => 'dd MM yyyy',
+                'data'  =>  array('year' => $dt->format('Y'), 'month' => $dt->format('m'), 'day' => $dt->format('d'))
+            ])
             ->add('content', TextareaType::class, [
-                'attr' => array('style' => 'width: 100%','rows' => '15')])
+                'attr' => array('style' => 'width: 100%','rows' => '15','placeholder' => 'Write here')])
         ;
     }
 
